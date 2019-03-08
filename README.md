5243fcbedcd8455bbe234a65c05fd03d27011997
NetworkRanger <17346503142@163.com>
2019-03-08 16:03:05 +0800

    ADD: 完成gitlab-markdown-index工具的编写

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # gitlab-markdown-index
-gitlab文档目录自动生成工具
+> gitlab文档目录自动生成工具
+
+此node模块是一个命令行工具，它遍历所有markdown文件的给定目录（扩展名为`.md`），并将提取所有内容的目录，输出至`stdout`。
+
+## 安装
+
+全局安装markdown-index。
+
+```
+npm install -g gitlab-markdown-index
+```
+
+## 用法
+
+给gitlab-markdown-index一个要遍历的目录，并将索引的内容输出到`stdout`。
+
+```
+markdown-index directory/fulla/markdowns > index.md
+```
+
+## 协议
+
+MIT
---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const toc = require('markdown-toc');
+const dir = process.argv[2];
+
+if (!dir) {
+    console.log('\nUsage: \nmarkdown-index directory/fulla/markdowns\n');
+    process.exit(1);
+}
+
+glob(dir + '/**/*.md', function(err, files) {
+    if (err) throw err;
+    var tables = files.map(function(file) {
+        if (file.match('index.md')) return;
+        if (file.match('README.md')) return;
+        if (file.match('node_modules')) return;
+
+        var filenameSlug = toc.slugify(path.parse(file).name);
+        var originalLinkify = toc.linkify;
+        var table = toc(fs.readFileSync(file, 'utf8'), {
+            filter: function(str, ele, arr) {
+                // Skip top-level elements if their slug matches a slug of the filename.
+                return ele.level != 1 || ele.slug != filenameSlug;
+            },
+            linkify: function(tok, text, slug, opts) {
+                // Use empty options arg to avoid infinite recursion.
+                var tok = originalLinkify(tok, text, slug, {});
+                // Prepend filename to links.
+                tok.content = tok.content.replace('#', file.replace(/\.md/i, '') + '#');
+                return tok;
+            },
+        });
+
+        // Add filename as a heading.
+        return '### [' + filenameSlug + '](' + file.replace(/\.md/i, '') + ')\n\n' + table.content;
+    });
+
+    process.stdout.write(tables.join('\n\n'));
+    return tables.join('\n\n');
+});
+
